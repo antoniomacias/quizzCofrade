b17b660906e56c9f8e4bf96748643b7a7715e656
antoniomacias <antmacmun@gmail.com>
2017-02-20 13:19:52 +0100

    Corrección errores

--- app/src/main/java/com/example/ammacias/quizzcofrade/RandomActivity.java
+++ app/src/main/java/com/example/ammacias/quizzcofrade/RandomActivity.java
@@ -1,5 +1,6 @@
 package com.example.ammacias.quizzcofrade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
@@ -9,6 +10,7 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.bcgdv.asia.lib.ticktock.TickTockView;
 import com.example.ammacias.quizzcofrade.localdb.DatabaseConnection;
 import com.example.ammacias.quizzcofrade.localdb.HermandadDB;
 import com.example.ammacias.quizzcofrade.localdb.HermandadDBDao;
@@ -16,6 +18,7 @@ import com.example.ammacias.quizzcofrade.localdb.PasosDB;
 import com.example.ammacias.quizzcofrade.localdb.PasosDBDao;
 import com.squareup.picasso.Picasso;
 
+import java.util.Calendar;
 import java.util.List;
 
 import cn.pedant.SweetAlert.SweetAlertDialog;
@@ -29,10 +32,33 @@ public class RandomActivity extends AppCompatActivity {
     EditText respuesta;
 
     String nombreRespuesta;
+
+    //Control aciertos/vidas
+    int var_global = 16;
+    int numVidas, numAciertos;
+    boolean bandera;
+    ImageView vida1;
+    ImageView vida2;
+    ImageView vida3;
+    ImageView vida4;
+    ImageView vida5;
+
+    com.bcgdv.asia.lib.ticktock.TickTockView mCountDown;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_random);
+
+        numVidas = 5;
+        numAciertos = 0;
+        vida1= (ImageView)findViewById(R.id.vida1);
+        vida2= (ImageView)findViewById(R.id.vida2);
+        vida3= (ImageView)findViewById(R.id.vida3);
+        vida4= (ImageView)findViewById(R.id.vida4);
+        vida5= (ImageView)findViewById(R.id.vida5);
+
+
 
         respuesta =(EditText)findViewById(R.id.respuesta_escudo);
 
@@ -42,6 +68,7 @@ public class RandomActivity extends AppCompatActivity {
         PasosDBDao PasosDBDao = DatabaseConnection.getPasosDBDao(this);
         listaP = PasosDBDao.loadAll();
 
+        //Check si acierta
         respuesta.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
@@ -63,6 +90,20 @@ public class RandomActivity extends AppCompatActivity {
         });
 
 
+        mCountDown = (TickTockView) findViewById(R.id.view_ticktock_countdown);
+        if (mCountDown != null) {
+            mCountDown.setOnTickListener(new TickTockView.OnTickListener() {
+                @Override
+                public String getText(long timeRemaining) {
+                    int seconds = (int) (timeRemaining / 1000) % 60;
+                    if (timeRemaining==0) next_escudo(findViewById(R.id.activity_detalle));
+                    return String.format("%1$02d",
+                            seconds,
+                            "s");
+                }
+            });
+        }
+
 
         imageView =(ImageView)findViewById(R.id.fotoDetalle);
 
@@ -82,6 +123,9 @@ public class RandomActivity extends AppCompatActivity {
 
     private void muestradialogo() {
         //guardarAcierto(hermandadDBDao.load(id_aux).getId());
+        mCountDown.stop();
+        numAciertos++;
+        bandera = true;
         new SweetAlertDialog(this, SweetAlertDialog.SUCCESS_TYPE)
                 .setTitleText("¡FLAMA HERMANO!")
                 .setContentText("¿Quieres pasar al siguiente nivel?")
@@ -97,31 +141,133 @@ public class RandomActivity extends AppCompatActivity {
     }
 
     public void next_escudo(View view) {
-        //Si es 0 -> Hermandad
-        //Si es 1 -> Paso
-        //Si es 2 -> Tunica
-        int aux = (int) (Math.random() * 3);
-        System.out.println("Numero: "+aux);
-        if (aux== 0){
-            Picasso.with(this)
-                    .load(listaH.get((int) (Math.random() * listaH.size())).getEscudo())
-                    .resize(250, 200)
-                    .into(imageView);
-            nombreRespuesta = listaH.get(0).getNombre();
-        }else if(aux ==1){
-            Picasso.with(this)
-                    .load(listaP.get((int) (Math.random() * listaP.size())).getFoto())
-                    .resize(250, 200)
-                    .into(imageView);
-            nombreRespuesta = listaP.get(0).getNombreTitular();
-        }else{
-            Picasso.with(this)
-                    .load("http://juegomarcas.esy.es/SS/images/ncage.jpg")
-                    .resize(250, 200)
-                    .into(imageView);
-            nombreRespuesta = listaH.get(0).getNombre();
+        //Reseteanis el campo
+        respuesta.setText("");
+
+        if (bandera == true);
+        else numVidas--;
+
+        if (numVidas==0){
+            finJuego();
+        }
+        else {
+            cambiarImg();
+
+            //Toast.makeText(this, "B: "+bandera, Toast.LENGTH_SHORT).show();
+            //Si es 0 -> Hermandad
+            //Si es 1 -> Paso
+            //Si es 2 -> Tunica
+            int aux = (int) (Math.random() * 3);
+            if (aux == 0) {
+                int randomInt = (int) (Math.random() * listaH.size());
+                Picasso.with(this)
+                        .load(listaH.get(randomInt).getEscudo())
+                        .resize(250, 200)
+                        .into(imageView);
+                nombreRespuesta = listaH.get(randomInt).getNombre();
+                System.out.println(nombreRespuesta);
+            } else if (aux == 1) {
+                int randomInt = (int) (Math.random() * listaP.size());
+                Picasso.with(this)
+                        .load(listaP.get(randomInt).getFoto())
+                        .resize(250, 200)
+                        .into(imageView);
+                nombreRespuesta = listaP.get(randomInt).getNombreTitular();
+                System.out.println(nombreRespuesta);
+            } else {
+                Picasso.with(this)
+                        .load("http://juegomarcas.esy.es/SS/images/ncage.jpg")
+                        .resize(250, 200)
+                        .into(imageView);
+                nombreRespuesta = listaH.get(0).getNombre();
+                System.out.println(nombreRespuesta);
+            }
+            bandera = false;
+            cuentaAtras();
         }
     }
 
+    private void cambiarImg() {
+        if (numVidas==4){
+            vida1.setImageResource(R.drawable.star);
+            vida2.setImageResource(R.drawable.star);
+            vida3.setImageResource(R.drawable.star);
+            vida4.setImageResource(R.drawable.star);
+            vida5.setImageResource(R.drawable.emptystar);
+        }else if(numVidas==3){
+            vida1.setImageResource(R.drawable.star);
+            vida2.setImageResource(R.drawable.star);
+            vida3.setImageResource(R.drawable.star);
+            vida4.setImageResource(R.drawable.emptystar);
+            vida5.setImageResource(R.drawable.emptystar);
+        }else if(numVidas==2){
+            vida1.setImageResource(R.drawable.star);
+            vida2.setImageResource(R.drawable.star);
+            vida3.setImageResource(R.drawable.emptystar);
+            vida4.setImageResource(R.drawable.emptystar);
+            vida5.setImageResource(R.drawable.emptystar);
+        }else if(numVidas==1) {
+            vida1.setImageResource(R.drawable.star);
+            vida2.setImageResource(R.drawable.emptystar);
+            vida3.setImageResource(R.drawable.emptystar);
+            vida4.setImageResource(R.drawable.emptystar);
+            vida5.setImageResource(R.drawable.emptystar);
+        }else if(numVidas==0) {
+            vida1.setImageResource(R.drawable.emptystar);
+            vida2.setImageResource(R.drawable.emptystar);
+            vida3.setImageResource(R.drawable.emptystar);
+            vida4.setImageResource(R.drawable.emptystar);
+            vida5.setImageResource(R.drawable.emptystar);
+        }
+    }
+
+    private void finJuego() {
+        // Poner el reloj a 0
+        mCountDown.stop();
+        cambiarImg();
+        //TODO: Cambiar img SweetAlertDialog
+        new SweetAlertDialog(this, SweetAlertDialog.WARNING_TYPE)
+                .setTitleText("¡SE HAN ACABADO LAS VIDAS!")
+                .setConfirmText("Volver a jugar")
+                .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                    @Override
+                    public void onClick(SweetAlertDialog sDialog) {
+                        sDialog.dismissWithAnimation();
+                        redirect(findViewById(R.id.activity_detalle));
+                    }
+                })
+                .show();
+
+        //TODO: Insertar los datos
+
+
+    }
+
+    private void redirect(View viewById) {
+        Intent i = new Intent(this, MainActivity.class);
+        startActivity(i);
+    }
+
+    void cuentaAtras(){
+        Calendar end = Calendar.getInstance();
+        end.add(Calendar.MINUTE, 0);
+        end.add(Calendar.SECOND, 16);
+
+        Calendar start = Calendar.getInstance();
+        start.add(Calendar.SECOND, -1);
+        if (mCountDown != null) {
+            mCountDown.start(start, end);
+        }
+    }
+
+    //https://github.com/CardinalNow/Android-CountdownTimer
+    //https://github.com/BCGDV-ASIA/android-widget-ticktock
+
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        cuentaAtras();
+    }
 
 }
